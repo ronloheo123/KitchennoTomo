679b9a7523baeb9d97ab84c98cd77fe836057e19
TinNguyen <tin.ntr.62cntt@ntu.edu.vn>
2025-08-15 01:38:45 +0700

    Fixed Layout

--- app/src/main/java/com/example/kitchennotomo/AddActivity.java
+++ app/src/main/java/com/example/kitchennotomo/AddActivity.java
@@ -5,6 +5,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddActivity extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class AddActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add); // layout chứa 2 EditText và nút ADD
 
         foodName = findViewById(R.id.foodname);
